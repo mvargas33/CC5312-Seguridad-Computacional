e2a4a1f3866e4049d24822ff049fd77c650155d9
pabtorres <pablo.torres.g@ug.uchile.cl>
2020-04-25 01:07:17 -0400

    Added get_p_q to findprimes algorithm

--- Lab1/P1/get_p_q_rsa.py
+++ Lab1/P1/get_p_q_rsa.py
@@ -4,6 +4,16 @@ from decimal import *
 getcontext().prec = 4096
 
 def get_p_q(N):
+    """
+    Return p and q from consecutives primes p, q vulnerability
+
+    :param N : Integer which corresponds to p*q
+    :type N : Int
+    
+    :returns: A tuple with p and q
+    :rtype: Tuple
+
+    """
     sqrt_N = int(Decimal(N).sqrt().to_integral_exact())
     p = next_prime(sqrt_N)
     q = Decimal(N)/Decimal(p)
