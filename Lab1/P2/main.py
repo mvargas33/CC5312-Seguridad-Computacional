5db3196f7fd3402cb2ec718a97df1777a190d63f
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2020-04-26 12:40:28 -0400

    Parte b lista

--- Lab1/P2/main.py
+++ Lab1/P2/main.py
@@ -3,10 +3,20 @@ import socket
 # We connect to a (host,port) tuple
 import utils
 
-CONNECTION_ADDR = ("cc5312.xor.cl", 5312)
+ADDRESS_A = ("cc5312.xor.cl", 5312)
+ADDRESS_B = ("cc5312.xor.cl", 5313)
+sock_A = utils.create_socket(ADDRESS_A)
+sock_B = utils.create_socket(ADDRESS_B)
+
+def senAResendB(message):
+    resp_A = utils.send_message(sock_A, message)
+    resp_B = utils.send_message(sock_B, resp_A)
+    return resp_B
+
+# CONNECTION_ADDR = ("cc5312.xor.cl", 5313)
 
 if __name__ == "__main__":
-    sock = utils.create_socket(CONNECTION_ADDR)
+    # sock = utils.create_socket(CONNECTION_ADDR)
     while True:
         try:
 
@@ -14,7 +24,8 @@ if __name__ == "__main__":
             response = input("send a message: ")
             # You need to use encode() method to send a string as bytes.
             print("[Client] \"{}\"".format(response))
-            resp = utils.send_message(sock, response)
+            # resp = utils.send_message(sock, response)
+            resp = senAResendB(response)
             print("[Server] \"{}\"".format(resp))
             # Wait for a response and disconnect.
         except Exception as e:
