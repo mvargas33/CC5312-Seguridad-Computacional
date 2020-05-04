bb749b2e0fb42a9935539440d82a0b8d17049f2a
mvargas33 <maximiliano.vargas@ug.uchile.cl>
2020-05-04 00:41:49 -0400

    Comentarios, y fix de n-1 a n-2 en decifrar un bloque

--- Lab1/P2/main.py
+++ Lab1/P2/main.py
@@ -72,7 +72,7 @@ def decode_last_char(c_text, block_size):
     Retorna el último byte del mensaje original del texto cifrado
     :param c_text: byte-like message
     """
-    error_mssg = "pkcs7: invalid padding" # TODO: Find a way to capture error message for invalid padding
+    error_mssg = "pkcs7: invalid padding"                   # Prefijo de error típico de padding
     blocks_array = utils.split_blocks(c_text, block_size//8)# Get cyphered text as an bytearray
     n = len(blocks_array)                                   # Cantidad n de bloques
     b = block_size//8                                       # Cantidad b de bytes por bloque
@@ -88,30 +88,31 @@ def decode_last_char(c_text, block_size):
         modified_c_text = utils.bytes_to_hex(utils.join_blocks(blocks_array)) # Joinblocks and then cast to hex
         resp = utils.send_message(sock_B_input, sock_B_output, modified_c_text) # Send to sock_B
         if not resp.startswith(error_mssg):                 # Check if there is not a padding error, we have a candidate
-            # Validar: Asegurar que texto plano termina en 0x01
-            ant = m_n1[b-2]             # M[n-1][b-2] penúltimo valor antiguo
-            m_n1[b-2] = ant+1 % 256     # Cambiar a otro valor
-            blocks_array[n-2] = m_n1    # Modificamos M[n-2]
+                                                            # Validar: Asegurar que texto plano termina en 0x01
+            ant = m_n1[b-2]                                 # M[n-1][b-2] penúltimo valor antiguo
+            m_n1[b-2] = ant+1 % 256                         # Cambiar a otro valor
+            blocks_array[n-2] = m_n1                        # Modificamos M[n-2]
             modified_c_text = utils.bytes_to_hex(utils.join_blocks(blocks_array))   # Joinblocks and then cast to hex
             resp = utils.send_message(sock_B_input, sock_B_output, modified_c_text) # Acá tenemos que el texto descifrado es del estilo [.....[0xfe][0x01]]
 
-            if resp == error_mssg:      # Si no validó estamos en un caso donde podríamos haber encontrado un falso positivo, ej: que el último byte descifrado fuera [0x02] y el byte anterior cifrado [0x02]. Ahora da [...[0xfe][0x02]] y no pasa
+            if resp == error_mssg:                          # Si no validó estamos en un caso donde podríamos haber encontrado un falso positivo, ej: que el último byte descifrado fuera [0x02] y el byte anterior cifrado [0x02]. Ahora da [...[0xfe][0x02]] y no pasa
                 print("No valida, valor encontrado para M[n-1][b-1] incosistente, buscando otro valor ...")
-                m_n1[b-2] = ant             # Always Revert al valor C[n-1][b-2] original
+                m_n1[b-2] = ant                             # Always Revert al valor C[n-1][b-2] original
                 blocks_array[n-2] = m_n1
                 modified_c_text = utils.bytes_to_hex(utils.join_blocks(blocks_array))
             else:
-                m_n1[b-2] = ant             # Always Revert
+                m_n1[b-2] = ant                             # Always Revert
                 blocks_array[n-2] = m_n1
                 modified_c_text = utils.bytes_to_hex(utils.join_blocks(blocks_array))
-                break # Pasó validación, encontramos M[n-1][b-1]
-        i+=1                                                # Try next i
+                break                                       # Pasó validación, encontramos M[n-1][b-1]
+        i+=1
         if(i == 256):
             print("Se han probado los 256 valores de padding sin éxito")
             exit(1)
     
     i_n = i^1 # XOR para obtener I_[n][b-1]. i = M[n-1][b-1], 1 = 0x01
     c_n1_b1 = utils.split_blocks(c_text, block_size//8)[n-2][b-1] # Get clean C[n-2][b-1]
+    print(i_n^c_n1_b1)
     return i_n, i_n^c_n1_b1 # XOR para obtener B_[n][b-1]
 
 def decode_last_block2(c_text, block_size, i_n_b1):
@@ -180,12 +181,13 @@ def decode_last_block2(c_text, block_size, i_n_b1):
         queremos -= 1
 
     # Obtener el último bloque
-    c_n1 = utils.split_blocks(c_text, block_size//8)[n-2]   # Get clean C[n-2][b-1]
-    b_n = bytearray(b)                                      # Crea bytearray de largo 128//8 = 16 bytes
-    for i in range(0, b - 1):                               # Copia del byte 0 al 15
+    c_n1 = utils.split_blocks(c_text, block_size//8)[n-2]       # Get clean C[n-2][b-1]
+    b_n = bytearray(b)                                          # Crea bytearray de largo 128//8 = 16 bytes
+    for i in range(0, b - 1):                                   # Copia del byte 0 al 15
         b_n[i] = i_n[i]^c_n1[i]
+    print(b_n)
     print(binascii.unhexlify(b_n.hex()))
-    return b_n                                              # Return bytearray del texto descifrado
+    return b_n                                                  # Return bytearray del texto descifrado
 
 
 def decode_all_blocks2(c_text, block_size):
@@ -193,8 +195,8 @@ def decode_all_blocks2(c_text, block_size):
     Dado un texto cifrado y tamaño de bloque,
     Retorna el texto plano del texto cifrado
     """
-    blocks_array = utils.split_blocks(c_text, block_size//8)# Get cyphered text as an bytearray
-    n = len(blocks_array)                                   # Cantidad n de bloques
+    blocks_array = utils.split_blocks(c_text, block_size//8)    # Get cyphered text as an bytearray
+    n = len(blocks_array)                                       # Cantidad n de bloques
     plain_text = bytearray()
 
     for i in range(n-1, 0, -1):
@@ -218,18 +220,17 @@ if __name__ == "__main__":
     block_size = 16*8
     while True:
         try:
-            # Read a message from standard input
-            response = input("send a message: ")
-            # The next line is a good hint
-            # You need to use encode() method to send a string as bytes.
+            
+            response = input("send a message: ") # Read a message from standard input
             print("[Client] \"{}\"".format(response))
-            # resp = utils.send_message(sock, response)
+
             resp = utils.send_message(sock_A_input, sock_A_output, response)
             decode_all_blocks2(resp.encode(), block_size)
+
             #i_n_b1, b = decode_last_char(resp.encode(), block_size)
             #decode_last_block2(resp.encode(), block_size, i_n_b1)
+
             print("[Server] \"{}\"".format(resp))
-            # Wait for a response and disconnect.
         except Exception as e:
             print(e)
             print("Closing...")
